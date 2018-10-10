31a5d1f2dd8f6bd0b9420d0e6b76192115be6dbc
李叶鑫 <liyexin@liyexindeMacBook-Pro.local>
2018-10-10 15:05:58 +0800

    webnpack打包初体验

---
+++ webpack_test/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/app.js',
+	output: {
+		path: path.resolve(__dirname,'dist'),
+		filename: 'main.js'
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	]
+};
